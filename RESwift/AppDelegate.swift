6fa60500d512c8325e71498aeaa8fa9ecaf72c06
Nikola-K <nikolak@outlook.com>
2015-10-25 09:48:03 +0100

    Initial commit

---
+++ RESwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RESwift
+//
+//  Created by Nikola Kovacevic on 25/10/15.
+//  Copyright © 2015 Nikola Kovacevic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RESwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RESwift
+//
+//  Created by Nikola Kovacevic on 25/10/15.
+//  Copyright © 2015 Nikola Kovacevic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
